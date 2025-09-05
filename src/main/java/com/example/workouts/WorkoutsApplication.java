812872fa01bc6ec1de2b800354596407305824c9
Rakesh <rakeshpatoju113@gmail.com>
2025-09-05 08:55:46 +0530

    all Beans available in application

--- src/main/java/com/example/workouts/WorkoutsApplication.java
+++ src/main/java/com/example/workouts/WorkoutsApplication.java
@@ -2,12 +2,16 @@ package com.example.workouts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class WorkoutsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkoutsApplication.class, args);
+		ConfigurableApplicationContext configurableApplicationcontext = SpringApplication.run(WorkoutsApplication.class, args);
+		Arrays.stream(configurableApplicationcontext.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 }
